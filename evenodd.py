80de03c74220e8031e1eabab520e8995f6c826b6
assimnt <35533773+assimnt@users.noreply.github.com>
2019-06-16 17:49:01 +0500

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,13 @@
+def no_check(a)
+if num % 2 == 0:
+    s=0
+    else:s
+        
+        n=1
+        return s
+num = int(input("Enter a number: "))
+x=no_check(a)
+if x == 0:
+   print("a, is Even".format(num))
+else:
+   print("a, is Odd".format(num))
